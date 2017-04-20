786ea54acaa648ca4e2b2c60381bd036dcaa5a20
Evgeniy Kuzorov <zhenya.kuzorov@finsight.com / kuzorov@gmail.com>
2017-04-20 15:16:21 +0300

    Unnecessary space removed

--- scripts/fonts/configure.sh
+++ scripts/fonts/configure.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 find ./assets/fonts/Meslo-Font -name '*.zip' -exec sh -c 'unzip -o -d "${0%.*}" "$0"' '{}' ';'
-find ./assets/fonts/Meslo-Font -name '*.ttf'  -ctime -15 -exec cp {} ~/Library/Fonts/ \;
+find ./assets/fonts/Meslo-Font -name '*.ttf' -ctime -15 -exec cp {} ~/Library/Fonts/ \;
